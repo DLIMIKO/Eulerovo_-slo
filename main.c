9e69a509930806b78636c212f5a7102f20a80cea
DLIMIKO <matus.necas@gmail.com>
2020-10-05 17:34:14 +0200

    + meranie času

--- main.c
+++ main.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
+#include <time.h>                                   // hlavičkový súbor potrebný na meranie času
 
 int main()
 {
 
-   int n = 1;                                        // iter. číslo
-   double eN = 1;                                    // hodnota e pre danú iter.
-   double factN1 = 1;                                // hodnota prírastku pre n+1-vú iter. , čiže (n+1)!
+   printf("\nProgram na aproximovanie eulerovho čísla:\n"); // úvodný text
+   clock_t start, stop;
+   double cas_programu;
+   int n = 1;                                           // iter. číslo
+   double eN = 1;                                       // hodnota e pre danú iter.
+   double factN1 = 1;                                   // hodnota prírastku pre n+1-vú iter. , čiže (n+1)!
 
-   printf("n\teN\t\t\t\t 1/factN1\n");               // hlavička výstupu
+   printf("n\teN\t\t\t\t 1/factN1\n");                  // hlavička výstupu
 
 
-  while(1 / factN1 > 1e-10)                          // cyklus pracuje, pokiaľ je prírastok väčší ako 1e-10
+  start=clock();                                        // časová značka, začiatok merania času
+
+  while(1 / factN1 > 1e-10)                             // cyklus pracuje, pokiaľ je prírastok väčší ako 1e-10
    {
-      printf("%d",n);                                // vypíše sa hodnota danej iterácie
-      eN = eN + (1 / factN1);                        // k hodnote eN sa pripočíta prírastok
-      n = n + 1;                                     // hodnota n sa navýši o 1
-      factN1 = factN1 * n;                           // povýšenie faktoriálu
-      printf("\t%.12f \t %.11f \n", eN, 1 / factN1); // vypíše sa číslo iteráie na 12 des. miest a prírastok na 11 des. miest
+      printf("%d",n);                                   // vypíše sa hodnota danej iterácie
+      eN = eN + (1 / factN1);                           // k hodnote eN sa pripočíta prírastok
+      n = n + 1;                                        // hodnota n sa navýši o 1
+      factN1 = factN1 * n;                              // zväčšenie faktoriálu n-krát
+      printf("\t%.12f \t %.11f \n", eN, 1 / factN1);    // vypíše sa číslo iteráie na 12 des. miest a prírastok na 11 des. miest
    }
 
+  stop=clock();                                         // časová značka, koniec merania času
+
+  printf("\nEulerovo číslo aproximované na 10 platných desatinných miest: %.10f\n",eN); //finálny výpis čísla e
+  printf("Čas procesu: %fs",((double)(stop-start))/CLOCKS_PER_SEC); //výpis doby trvania v sekundách
     return 0;
 }
